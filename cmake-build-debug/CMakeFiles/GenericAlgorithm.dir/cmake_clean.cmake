0ce850bdae923f7075cbc3f079cad5601a95d580
A00992390 <wangkang970111@gmail.com>
2019-11-28 21:21:36 -0800

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/GenericAlgorithm.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/GenericAlgorithm.dir/main.cpp.o"
+  "GenericAlgorithm.exe"
+  "GenericAlgorithm.pdb"
+  "libGenericAlgorithm.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/GenericAlgorithm.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/GenericAlgorithm.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for GenericAlgorithm.
+# This may be replaced when dependencies are built.
